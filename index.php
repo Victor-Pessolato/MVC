5e72b4954e1e0174019810934af3d06738b23b35
Victor <>
2018-09-03 11:21:10 +0200

    Created home.php pt.2

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        require_once 'views/home.php';
-        ?>
-    </body>
-</html>
+<?php
+
+require_once 'views/home.php';
