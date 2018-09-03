7c31a0e12a9df8d3fc69935db000f541484b06bb
Victor <>
2018-09-03 11:20:23 +0200

    Created home.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,8 +6,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        require_once("db/db.php");
-        require_once("controllers/actores_controller.php");
+        require_once 'views/home.php';
         ?>
     </body>
 </html>
---
+++ views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sakila</title>
+    </head>
+    <body>
+        <h1>MVC desde cero - Sakila web</h1>
+        <h2>Bienvenidos a este humilde programa que nos permitirá conocer los arcanos del cine</h2>
+    </body>
+</html>
