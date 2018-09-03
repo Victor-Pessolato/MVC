557929559426d2ca7e085d6a2ba56928ccab207b
Victor <>
2018-09-03 14:03:41 +0200

    Created views for films

---
+++ views/film_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Film</title>
+    </head>
+    <body>
+        <p>ID:<?= $dato["film_id"] ?></p>
+        <p>Title:<?= $dato["title"] ?></p>
+        <p>Description:<?= $dato["description"] ?></p>
+        <p>Last Update:<?= $dato["last_update"] ?></p>
+    </body>
+</html>
---
+++ views/films_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Peliculas de Sakila</title>
+    </head>
+    <body>
+        <?php
+        foreach ($datos as $dato) {
+            //echo $dato["first_name"] . " " . $dato["last_name"] . " <a href='index.php?controller=actores&action=view&id="
+            //. $dato["actor_id"] . "'>Detalle</a> " . "<br/>";
+            echo $dato["title"] . " " . $dato["description"] . " ";
+            ?>
+            <a href="index.php?controller=actores&action=view&id=<?= $dato["film_id"] ?>">Detalle</a> <br/>
+            <?php
+        }
+        ?>
+    </body>
+</html>
