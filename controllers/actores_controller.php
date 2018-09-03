3ea26dbd5eb2652241eed29359b14c75aa5cc612
Victor <>
2018-09-03 10:17:36 +0200

    Created actores_controller.php

---
+++ controllers/actores_controller.php
@@ -0,0 +1,9 @@
+<?php
+
+//Llamada al modelo
+require_once("models/actores_model.php");
+$per = new actores_model();
+$datos = $per->get_actores();
+
+//Llamada a la vista
+require_once("views/actores_view.php");
--- index.php
+++ index.php
@@ -12,10 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         require_once("db/db.php");
-        require_once("models/actores_model.php");
-
-        $a = new actores_model();
-        print_r($a->get_actores());
+        require_once("constrollers/actores_controller.php");
         ?>
     </body>
 </html>
